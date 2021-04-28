8890413bb5c32666ae88d3556f54897bd7a66ecd
Timothy Butler <tbutler1132@gmail.com>
2021-04-28 11:10:51 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+const BASE_API = 'http://localhost:5000'
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(false)
+
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+
+    if(token) {
+      fetch(`${BASE_API}/users/profile`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(r => r.json())
+      .then(data => {
+        console.log(data)
+        setCurrentUser(data)
+      })
+      .catch(error => console.log(error))
+    }
+  }, [])
+
+
+  const loginHandler = (userInfo) => {
+    fetch(`${BASE_API}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    })
+    .then(r => r.json())
+    .then(data => {
+      localStorage.setItem("token", data.accessToken)
+      setCurrentUser(data.user)
+    })
+  }
+
+  console.log(currentUser)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login loginHandler={loginHandler}/>
+      {currentUser ? 
+      <Profile currentUser={currentUser}/>
+      :
+      null}
     </div>
   );
+
 }
 
 export default App;
